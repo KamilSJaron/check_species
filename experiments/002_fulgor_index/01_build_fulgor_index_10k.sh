068598b18f14bc2e689c7fcd14468631a85d0b65
Karel Břinda <karel.brinda@gmail.com>
2024-11-12 16:15:49 +0100

    Fix fulgor construction script

--- experiments/002_fulgor_index/01_build_fulgor_index_10k.sh
+++ experiments/002_fulgor_index/01_build_fulgor_index_10k.sh
@@ -6,7 +6,5 @@ set -u
 #set -f
 #PS4='\[\e[32m\][$(date "+%Y-%m-%d %H:%M:%S") L${LINENO}]\[\e[0m\] '; set -x
 
-./fulgor build -t 8 -k 31 -m 20 -l <(xzcat seqs_10k/fof.txt.xz) -o index.fur
-
-
+./fulgor build -t 8 -k 31 -m 20 -l <(xzcat seqs_10k/fof.txt.xz | sed 's/^/seqs_10k\//') -o index_10k
 

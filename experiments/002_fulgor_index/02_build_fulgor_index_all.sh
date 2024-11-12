ac8d3d6439cf6bcb508e314d6a741f03fef1e09b
Karel Břinda <karel.brinda@gmail.com>
2024-11-12 16:19:24 +0100

    Build index for all sequences

--- experiments/002_fulgor_index/02_build_fulgor_index_all.sh
+++ experiments/002_fulgor_index/02_build_fulgor_index_all.sh
@@ -7,5 +7,5 @@ set -u
 #PS4='\[\e[32m\][$(date "+%Y-%m-%d %H:%M:%S") L${LINENO}]\[\e[0m\] '; set -x
 
 k=31
-./fulgor build -t 8 -k $k -m 20 -l <(xzcat seqs_10k/fof.txt.xz | sed 's/^/seqs_10k\//') -o "index_10k.k$k"
+./fulgor build -t 8 -k $k -m 20 -l <(xzcat seqs_all/fof.txt.xz | sed 's/^/seqs_all\//') -o "index_all.k$k"
 

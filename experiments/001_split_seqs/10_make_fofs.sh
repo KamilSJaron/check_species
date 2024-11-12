6bd10753c4fbcad74b19cb9ccc8a9b490ee77841
Karel Břinda <karel.brinda@gmail.com>
2024-11-12 12:19:22 +0100

    Add first fofs

--- experiments/001_split_seqs/10_make_fofs.sh
+++ experiments/001_split_seqs/10_make_fofs.sh
@@ -4,10 +4,10 @@ set -e
 set -o pipefail
 set -u
 
-find seqs_10k/ -name '*.fa' \
+find seqs_10k -name '*.fa' \
 	| xz -9 -T1 \
 	> seqs_10k.txt.xz
 
-find seqs_all/ -name '*.fa' \
+find seqs_all -name '*.fa' \
 	| xz -9 -T1 \
 	> seqs_all.txt.xz

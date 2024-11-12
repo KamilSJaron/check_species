de42cfe3e4d5dc612048be748927e66b019d6881
Karel Břinda <karel.brinda@gmail.com>
2024-11-12 12:44:15 +0100

    Fix ids

--- experiments/001_split_seqs/01_split_seqs__first10k.sh
+++ experiments/001_split_seqs/01_split_seqs__first10k.sh
@@ -4,15 +4,24 @@ set -e
 set -o pipefail
 set -u
 
-xzcat -v ibol.fa.xz \
-	| head -n 20000 \
-	| awk '/^>BOLD:/ {
-		split(substr($0, 2), a, "_")
-		sub(/^BOLD:/, "", a[1])
-		OUT = "seqs_10k/" a[1] ".fa"
-	}
-	{
-		print >> OUT
-		close(OUT)
-	}'
-
+(
+	cd seqs_10k
+	xzcat -v ../ibol.fa.xz \
+		| head -n 20000 \
+		| awk '/^>BOLD:/ {
+			split($0, parts, "BOLD:")
+			if (length(parts) >= 3) {
+				base = parts[3]
+				gsub(/^[ \t]+|[ \t]+$/, "", base)
+				gsub(/ /, "_", base)
+				OUT = base ".fa"
+			} else {
+				print "Warning: Line does not contain two BOLD: occurrences: " $0 > "/dev/stderr"
+				OUT = "unknown.fa"
+			}
+		}
+		{
+			print >> OUT
+			close(OUT)
+		}'
+)
